883ef69abd3b82f34d4d72e50b60f63ab5a4eefd
andrei.baltat <andrei.baltat@adswizz.com>
2023-07-16 08:02:17 +0300

    - switched from Long to int

--- src/main/java/com/example/meditatii/entities/Student.java
+++ src/main/java/com/example/meditatii/entities/Student.java
@@ -1,6 +1,5 @@
 package com.example.meditatii.entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,7 +7,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -24,7 +22,7 @@ public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Integer id;
     private String firstName;
     private String lastName;
     private Integer age;
