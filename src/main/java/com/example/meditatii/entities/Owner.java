cf64f32bd1d36fc252047b163ed97d3597148e79
andrei.baltat <andrei.baltat@adswizz.com>
2023-07-30 15:07:04 +0300

    - one to many - many to one

--- src/main/java/com/example/meditatii/entities/Owner.java
+++ src/main/java/com/example/meditatii/entities/Owner.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Entity
 @Data
@@ -18,4 +22,7 @@ public class Owner {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @OneToMany(mappedBy = "owner")
+    private Set<Pet> pet;
 }
--- src/main/java/com/example/meditatii/entities/Pet.java
+++ src/main/java/com/example/meditatii/entities/Pet.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,4 +22,8 @@ public class Pet {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "owners_id")
+    private Owner owner;
 }
