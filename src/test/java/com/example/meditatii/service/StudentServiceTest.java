315f863d23ec8438843fc0f71d75c0498c06c810
andrei.baltat <andrei.baltat@adswizz.com>
2023-08-17 14:14:20 +0300

    - latest

--- src/test/java/com/example/meditatii/service/StudentServiceTest.java
+++ src/test/java/com/example/meditatii/service/StudentServiceTest.java
@@ -4,13 +4,14 @@ import com.example.meditatii.DTO.StudentDTO;
 import com.example.meditatii.entities.Student;
 import com.example.meditatii.repositories.StudentRepository;
 import com.example.meditatii.services.StudentService;
+import jakarta.transaction.Transactional;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.verify;
@@ -25,15 +26,17 @@ public class StudentServiceTest {
     StudentService service;
 
     @Test
+    @Transactional // daca pica se face rollback
+    @Rollback // dupa ce se termina se face roll back
     public void testFindById(){
         Long id = 10L;
         Student student = Student.builder().age(10).lastName("Andrei").firstName("A").id(id).build();
         Mockito.doReturn(student).when(repository.findById(id));
         StudentDTO studentById = service.getStudentById(id);
 
-        verify(repository).findById(id);
-        assertNotNull(studentById);
-        assertEquals(id, studentById.getId());
+        verify(repository).findById(id);     // verifici ca s-a facut callul
+        assertNotNull(studentById);         // verifici ca nu e diferit de null
+        assertEquals(id, studentById.getId());// verifici ca s-a intors ce trebuie
     }
     /**
      * @Test
